64290c9ff03f413952c77282dcdc3377516a86dd
Vadim <unistars@mail.ru>
2020-05-19 16:01:58 +0300

    Updates

---
+++ precache-manifest.530384f00a99b598c7df9dc8fadc5b08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb0f7e9deebc995d8d84156f6c78bdc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e259a4dd2531e0370697",
+    "url": "/portfolio/static/css/main.19848260.chunk.css"
+  },
+  {
+    "revision": "bd2cc3d788df4a283997",
+    "url": "/portfolio/static/js/2.e523ef48.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.e523ef48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e259a4dd2531e0370697",
+    "url": "/portfolio/static/js/main.3724914b.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  }
+]);
